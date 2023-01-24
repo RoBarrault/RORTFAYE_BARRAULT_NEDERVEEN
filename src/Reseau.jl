873af14cc4cd322f335c3aef2dfceeafb2dd29c2
RoBarrault <romain.barrault@ensta-paris.fr>
2023-01-24 17:31:05 +0100

    Update Reseau.jl

--- src/Reseau.jl
+++ src/Reseau.jl
@@ -7,7 +7,7 @@ mutable struct Reseau
     Demands         :: Vector{Int64}                        #Dk
     Vertices        :: Vector{Int64}                        #V
     Branches        :: Vector{Tuple{Int64,Int64}}           #A
-    Costs           :: Vector{Int64}                        #c_a : de la même taille que 
+    Costs           :: Vector{Int64}                        #c_a : de la même taille que A
     Taxed_branches  :: Vector{Bool}                         #A1 (note : A2 = A\A1) : de la même taille que Branches, l'élément i est true Ssi l'arête i est dans A1
     Taxes           :: Vector{Int64}                        #T_a : de la même taille que Branches, l'élément i est égal à la taxe sur l'arête i
 
